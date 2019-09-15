a121970be3ee93cfcae152f8b413e9c5e7de7311
PatrykGrzegorek <patryk.grzegorek@o2.pl>
2019-09-15 07:21:03 +0200

    poranekPatryk

--- index.js
+++ index.js
@@ -16,11 +16,12 @@ fetch('http://127.0.0.1:8000/api/baza/')
 
 function artykul(id, tytul)
 {
-    document.getElementById('blocks').innerHTML += "<div onclick='fullartykul("+id+")' id ="+id+">"+tytul+"</div>"
+    document.getElementById('blocks').innerHTML += "<div onclick='fullartykul("+id+")' id ="+id+" class='front'>"+tytul+"</div>"
 }
 
 function fullartykul(id)
 {
+    document.getElementById('head').innerHTML = "";
     document.getElementById('blocks').innerHTML = "<div id='tytul'></div><div id='opis'></div>";
     fetch('http://127.0.0.1:8000/api/baza/'+id+'/',{headers:{'Origin':'http://127.0.0.1'}})
     .then(function(response){
