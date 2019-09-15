d41c18fbfe9b31820685cb4a7abdd3e474aef3d3
PatrykGrzegorek <patryk.grzegorek@o2.pl>
2019-09-15 05:41:55 +0200

    koniecPatryka

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ Zainstalowanie venv,
 1. Otwórz terminal
 2. Wejdz w terminalu do folderu z bazą danych do folderu Scripts (cd (nazwa folderu))
 3. Wpisz activate
-3.5. Wpisz pip install django
-3.75. Wpisz pip install djangorestframework
-3.9. Zainstaluj wsparcie do pythona na visual studio code
+3,5. Wpisz pip install django
+3,75. Wpisz pip install djangorestframework
+3,8. Wpisz pip install django-cors-headers
+3,9. Zainstaluj wsparcie do pythona na visual studio code
 4. Gotowe
--- api_envi/baza_envi/baza_envi/settings.py
+++ api_envi/baza_envi/baza_envi/settings.py
@@ -38,18 +38,44 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'baza',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOW_CREDENTIALS = True
+
+# CORS_ALLOW_METHODS = (
+#     'DELETE',
+#     'GET',
+#     'OPTIONS',
+#     'PATCH',
+#     'POST',
+#     'PUT',
+# )
+#
+# CORS_ALLOW_HEADERS = (
+#     'accept',
+#     'accept-encoding',
+#     'authorization',
+#     'content-type',
+#     'dnt',
+#     'origin',
+#     'user-agent',
+#     'x-csrftoken',
+#     'x-requested-with',
+# )
 
 ROOT_URLCONF = 'baza_envi.urls'
 
