f1b613449ce198f9a905e240ea8aeddccde527b3
PatrykGrzegorek <patryk.grzegorek@o2.pl>
2019-09-15 13:07:50 +0200

    poprawka

--- api_envi/baza_envi/baza/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Artykul(models.Model):
-    tytul = models.CharField(max_length=128)
-    opis = models.TextField()
-
-    def __str__(self):
-        return self.tytul
-
--- motywy.js
+++
@@ -1,9 +0,0 @@
-var motywy = document.querySelectorAll('.change-theme span');
-var root = document.querySelector(':root');
-
-motywy.forEach((swatch) => {
-   swatch.addEventListener('click', (e) => {
-      root.style.setProperty('--theme-color', e.target.style.background);
-   })
-})
-
